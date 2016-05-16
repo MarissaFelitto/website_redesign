f790af7c32bd30cc7c18c458c06d6a22bd2f14b6
Marissa Felitto <mfelitto1@gmail.com>
2016-05-16 16:27:26 -0400

    enable psuedo mobile hover

--- js/project.js
+++ js/project.js
@@ -1,7 +1,23 @@
 
+// menu toggle
 
 $(document).ready(function(){
     $(".contact").click(function(){
         $(".icons").slideToggle("fast");
     });
+});
+
+// hover mobile 
+
+$('.hover').on("touchstart", function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('.hover').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
 });
